49a1a69c7366d3ce6ab67a41b211a98530314b2c
stuBirdFly <1065492934@qq.com>
2026-02-10 14:02:29 +0800

    fix(hnsw): fix concurrent access to max_level_ in HNSW search operations

--- src/algorithm/hnswlib/hnswalg.cpp
+++ src/algorithm/hnswlib/hnswalg.cpp
@@ -1595,9 +1595,11 @@ HierarchicalNSW::searchKnn(const void* query_data,
                                                         iter_ctx);
     } else {
         int64_t currObj;
+        int max_level_copy;
         {
             std::shared_lock data_loc(max_level_mutex_);
             currObj = enterpoint_node_;
+            max_level_copy = max_level_;
         }
         if (currObj > cur_element_count_) {
             return result;
@@ -1605,7 +1607,7 @@ HierarchicalNSW::searchKnn(const void* query_data,
 
         float curdist = fstdistfunc_(query_data, getDataByInternalId(currObj), dist_func_param_);
         std::shared_ptr<char[]> link_data = std::shared_ptr<char[]>(new char[size_links_level0_]);
-        for (int level = max_level_; level > 0; level--) {
+        for (int level = max_level_copy; level > 0; level--) {
             bool changed = true;
             while (changed) {
                 changed = false;
@@ -1684,14 +1686,16 @@ HierarchicalNSW::searchRange(const void* query_data,
     std::shared_ptr<float[]> normalize_query;
     normalizeVector(query_data, normalize_query);
     int64_t currObj;
+    int max_level_copy;
     {
         std::shared_lock data_loc(max_level_mutex_);
         currObj = enterpoint_node_;
+        max_level_copy = max_level_;
     }
     float curdist = fstdistfunc_(query_data, getDataByInternalId(currObj), dist_func_param_);
 
     std::shared_ptr<char[]> link_data = std::shared_ptr<char[]>(new char[size_links_level0_]);
-    for (int level = max_level_; level > 0; level--) {
+    for (int level = max_level_copy; level > 0; level--) {
         bool changed = true;
         while (changed) {
             changed = false;
